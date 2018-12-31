deb9c8606076b7d2fa9d192cda6c790de0dff724
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2018-12-31 13:36:52 +0100

    Added messages[] property

--- parser.py
+++ parser.py
@@ -3,6 +3,12 @@ from io import StringIO
 from lxml import etree
 from lxml.cssselect import CSSSelector
 
+def getThumbnail(prop):
+    rawStyle = prop.get("style")
+    regex = re.compile("'(.*)'")
+    thumbnail = regex.search(rawStyle).group(1)
+    return thumbnail
+
 def parse(rawHtml):
     parser = etree.HTMLParser()
     document = etree.parse(StringIO(rawHtml), parser)
@@ -15,11 +21,7 @@ def parseNews(rawHtml):
 
     for newsItem in container:
         for prop in newsItem:
-            if prop.get("class") == "pubThumbnail":
-                rawStyle = prop.get("style")
-                regex = re.compile("'(.*)'")
-                thumbnail = regex.search(rawStyle).group(1)
-
+            if prop.get("class") == "pubThumbnail":   thumbnail = getThumbnail(prop)
             elif prop.get("class") == "pubDate":      date      = prop.text
             elif prop.get("class") == "pubCategory":  category  = prop.text
             elif prop.get("class") == "pubTitle":     title     = prop.text
@@ -40,5 +42,29 @@ def parseNews(rawHtml):
     return articleList
 
 def parseMessages(rawHtml):
-    return []
+    document = parse(rawHtml)
+    container = CSSSelector("div.pubItems")(document)[0]
+    messageList = list()
+
+    for messageItem in container:
+        for prop in messageItem:
+            if prop.get("class") == "pubThumbnail":  thumbnail = getThumbnail(prop)
+            elif prop.get("class") == "pubDate":     date      = prop.text
+            elif prop.get("class") == "pubCategory": category  = prop.text
+            elif prop.get("class") == "pubTitle":    title     = prop.text
+            elif prop.get("class") == "pubSummary":  summary   = prop.text
+            elif prop.get("class") == "pubLink":     url       = prop.get("href")
+
+        message = {
+            "thumbnail": thumbnail,
+            "date": date,
+            "category": category,
+            "summary": summary,
+            "title": title,
+            "url": url
+        }
+
+        messageList.append(message)
+
+    return messageList
 
